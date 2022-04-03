c3f7aba7fede3c2a1107a45de601e013e12b65ef
Sardor <101808413+sardorxidirov@users.noreply.github.com>
2022-04-03 18:17:21 +0500

    Create 14-dars. LUG'AT BILAN TANISHUV.py

---
+++ 14-dars. LUG'AT BILAN TANISHUV.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  3 18:11:01 2022
+
+@author: Pro
+"""
+
+
+
+
+### Amaliyot
+
+#01 otam (onam, akam, ukam, va hokazo) degan lug'at yarating
+    #va lug'atga shu inson haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo).
+    #Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring: Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+    
+#02 Oila a'zolaringizning sevimli taomlari lug'atini tuzing. 
+    #Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin.
+    #Kamida uch kishining sevimli taomini konsolga chiqaring: Alining sevimli taomi osh
+    
+#03 Python izohli lu'gati tuzing:
+    #Lug'atga shu kunga qadar o'rgangan 10 ta so'z (atamani) kiriting (masalan integer, float, string, if, else va hokazo)
+    #va har birining qisqacha tarjimasini yozing.
+    
+#04 Foydalanuvchidan biror so'z kiritishni so'rang 
+    #va so'zning tarjimasini yuqoridagi lug'atdan chiqarib bering.
+    #Agar so'z lu'gatda mavjud bo'lmasa, "Bunda so'z mavjud emas" degan xabarni chiqaring.
+    
+#05 Yuqoridagi vazifani if-else yordamida qiling 
+    #va natijani ham foydalanuvchiga tushunarli ko'rinishda chiqaring.
